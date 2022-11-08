30502340036994c6db358f49a6f1823dada6856c
shiqi <shiqi@indexed.cn>
2022-11-08 16:44:12 +0800

    feat: test

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "Missing semicolon.eslint": 0,
+        "react/react-in-jsx-scope": "off"
+    }
+}
